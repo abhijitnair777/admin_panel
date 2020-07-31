b4fe620373429228310d1ed0d059cb3539fbd9f3
Abhijeet <abnair@bestpeers.com>
2020-07-31 11:39:30 +0530

    Initial commit

---
+++ app/admin/books.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Book do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :price, :author_id, :genre_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :price, :author_id, :genre_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
