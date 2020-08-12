049b3a8f049a4f32370cf76bd50802c78dda5a7d
abhijeet <abnair@bestpeers.com>
2020-08-12 13:53:40 +0530

    Added internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'homepages#index'
   
-  resources :books, only: [:index, :show] do
-  	collection do
-  	  put 'show_multiple'
-  	end  
-  end	
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: 'homepages#index'
+  
+    resources :books, only: [:index, :show] do
+  	  collection do
+  	    put 'show_multiple'
+  	  end  
+    end	
+  end
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
